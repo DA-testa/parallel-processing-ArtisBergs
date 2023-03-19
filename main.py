08623fc1d6c9f169c69df8a46d7e4df1ea4aed57
ArtisBergs <bergs.artis@gmail.com>
2023-03-19 19:55:21 +0200

    v1 (again)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # python3
 
+
 def parallel_processing(n, m, data):
     output = []
     # TODO: write the function for simulating parallel tasks, 
@@ -16,14 +17,12 @@ def parallel_processing(n, m, data):
     for j in range(0, jobs):
         current = min(threads)
         currentIndex = threads.index(current)
-
         
         output.append([currentIndex, threads[currentIndex]])
         threads[currentIndex]=threads[currentIndex]+jobList[j]
-            
-
 
     return output
+
 
 def main():
     # TODO: create input from keyboard
@@ -42,12 +41,11 @@ def main():
     data = list(map(int, I.split()))
 
     # TODO: create the function
-    result = parallel_processing(n,m,data)
+    result = parallel_processing(n, m, data)
     
     # TODO: print out the results, each pair in it's own line
     for i, j in result:
         print(i, j)
-
 
 
 if __name__ == "__main__":
